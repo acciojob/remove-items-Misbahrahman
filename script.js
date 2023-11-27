262fd898c0f97b72e4ae9f40a777aefec8731900
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-11-27 13:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+let ele = document.getElementById("colorSelect");
+let btn = document.getElementById("s");
+
+btn.addEventListener("click", function() {
+
+	
+    if (ele.value === "Red") {
+        document.getElementById("1").remove();
+    }
+    if (ele.value === "Green") {
+        document.getElementById("2").remove();
+    }
+    if (ele.value === "White") {
+        document.getElementById("3").remove();
+    }
+    if (ele.value === "Black") {
+        document.getElementById("4").remove();
+	}
+
+	
+});
